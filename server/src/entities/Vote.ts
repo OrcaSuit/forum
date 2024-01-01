a47782cac51eeae8a1e828480e1219ae53f079aa
김건우 <orcasuit@gmail.com>
2024-01-01 19:54:33 +0900

    빠진 내용 수정

--- server/src/entities/Vote.ts
+++ server/src/entities/Vote.ts
@@ -1,11 +1,28 @@
-import { Column, Entity } from "typeorm";
-import { BaseEntity } from './Entity';
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { BaseEntity } from "./Entity";
 
-@Entity("votes")
-export default class Post extends BaseEntity {
+@Entity("vote")
+export class User extends BaseEntity {
     @Column()
     value: number;
 
     @ManyToOne(() => User)
+    @JoinColumn({ name: "username", referencedColumnName: "username" })
+    user: User
+
+    @Column()
+    username: string;
+
+    @Column({ nullable: true })
+    postId: number;
+
+    @ManyToOne(() => Post)
+    post: Post;
+
+    @Column({ nullable: true })
+    commentId: number;
+
+    @ManyToOne(() => Comment)
+    comment: Comment
 
 }
