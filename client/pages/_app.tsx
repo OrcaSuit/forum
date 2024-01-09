8b85a0bd19838a9ad3e7cf8620e845868f950ac0
김건우 <orcasuit@gmail.com>
2024-01-10 01:12:49 +0900

    로그아웃 기능 추가

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,14 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Axios from 'axios'
 import { AuthProvider } from '@/src/context/auth';
+import NavBar from '@/src/components/NavBar';
+import { useRouter } from 'next/router';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + "/api";
   Axios.defaults.withCredentials = true;
 
+  const { pathname } = useRouter();
+  const authRoutes = ["/register", "/login"];
+  const authRoute = authRoutes.includes(pathname);
 
-  return (<AuthProvider>  <Component {...pageProps} /> </AuthProvider >)
+  return <AuthProvider>
+    {!authRoute && <NavBar />}
+    <div className={authRoute ? "" : "pt-12"}>
+      <Component {...pageProps} />
+    </div >
+  </AuthProvider >
 }
 
 export default MyApp
